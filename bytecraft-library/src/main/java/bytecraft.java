2756950eb56695e3ff1e014a4e1c317affd2ea4b
bluelhf <ilari.suhonen@gmail.com>
2026-01-01 23:05:41 +0200

    chore(standard-library): remove unused wrap_as_command
    not needed anymore because we have the command section

--- bytecraft-library/src/main/java/bytecraft.java
+++ bytecraft-library/src/main/java/bytecraft.java
@@ -1,13 +1,3 @@
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
-
-import java.util.function.Function;
-
 public final class bytecraft {
     private bytecraft() {}
-
-    public static Command<CommandSourceStack> wrap_as_command(final Function<CommandContext<CommandSourceStack>, Integer> function) {
-        return function::apply;
-    }
 }
