0fcb74b3c698a77001b32e01242db34653095346
bluelhf <ilari.suhonen@gmail.com>
2026-02-24 01:36:37 +0200

    chore(commands): javadoc

--- bytecraft-library/src/main/java/blue/lhf/bytecraft/library/commands/tree/CommandData.java
+++ bytecraft-library/src/main/java/blue/lhf/bytecraft/library/commands/tree/CommandData.java
@@ -1,5 +1,16 @@
 package blue.lhf.bytecraft.library.commands.tree;
 
+import org.byteskript.skript.compiler.structure.BasicTree;
+import org.byteskript.skript.compiler.structure.SectionMeta;
+
+/**
+ * Data related to a command tree, such as the command tree's root name, context variable name, arguments, and so on.
+ * This is used to construct the code that registers the command with Brigadier.
+ * <p>
+ * These are recorded as additional data on the command member's {@link SectionMeta}. They <i>should</i> be a {@link BasicTree},
+ * but are not because the trigger holder erroneously clears all trees instead of only its child trees when it exits.
+ * @see MemberCommand
+ * */
 public class CommandData {
     private final CommandDetails details;
 
